e65f9dee303713981a7d8cb437d3ad5f1f34c674
Florian Asche <git@florian-asche.de>
2025-08-07 00:44:39 +0200

    switched default to pipewire audio device

--- 02-stage-audiodriver-respeaker_lite/01-keep-audio-alive/files/keep-audio-alive.sh
+++ 02-stage-audiodriver-respeaker_lite/01-keep-audio-alive/files/keep-audio-alive.sh
@@ -4,4 +4,4 @@
 export XDG_RUNTIME_DIR=/run/user/1000
 
 # run silent output
-sox -n -r 16000 -c 1 -b 16 -e signed-integer -t alsa default synth 0 sine 0 vol 0.0
+sox -n -r 16000 -c 1 -b 16 -e signed-integer -t alsa pipewire synth 0 sine 0 vol 0.0
