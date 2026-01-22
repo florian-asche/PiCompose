230f3270a5a653fa9035fa7657569e8fd34c172a
Florian Asche <git@florian-asche.de>
2026-01-22 10:56:36 +0100

    improove systemd services

--- 01-stage-picompose/04-set-audio-volume/files/configure_audio.sh
+++ 01-stage-picompose/04-set-audio-volume/files/configure_audio.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-# Funktion zum Setzen der Audio-Einstellungen
-configure_audio() {
-    if amixer -c seeed2micvoicec info >/dev/null 2>&1; then
-        echo "seeed2micvoicec found"
-        amixer -c seeed2micvoicec set Headphone 100%
-        amixer -c seeed2micvoicec set Speaker 100%
-    elif amixer -c Lite info >/dev/null 2>&1; then
-        echo "Lite found"
-        amixer -c Lite set Headphone 100%
-        amixer -c Lite set Speaker 100%
-        amixer set Master 100%
-    else
-        exit 1
-    fi
-
-    alsactl store
-    touch /var/lib/alsa/audio_configured
-    # Deaktiviere den Service nach erfolgreicher Konfiguration
-    systemctl disable configure_audio.service
-
-}
-
-# Wenn die Konfiguration bereits erfolgreich war, beenden
-if [ -f /var/lib/alsa/audio_configured ]; then
-    exit 0
+if amixer -c seeed2micvoicec info >/dev/null 2>&1; then
+    echo "seeed2micvoicec found"
+    amixer -c seeed2micvoicec set Headphone 100%
+    amixer -c seeed2micvoicec set Speaker 100%
+elif amixer -c Lite info >/dev/null 2>&1; then
+    echo "Lite found"
+    amixer -c Lite set Headphone 100%
+    amixer -c Lite set Speaker 100%
+    amixer set Master 100%
 else
-    # Versuche die Audio-Einstellungen zu setzen
-    configure_audio
+    exit 1
 fi
+
+alsactl store
--- 01-stage-picompose/05-set-hostname/files/configure_hostname.sh
+++ 01-stage-picompose/05-set-hostname/files/configure_hostname.sh
@@ -1,49 +1,31 @@
 #!/bin/bash
 
-# Funktion zum Setzen der Audio-Einstellungen
-configure_hostname() {
+# Aktives Interface ermitteln
+IFACE=$(ip route | grep default | awk '{print $5}' | head -n 1)
 
-    # Aktives Interface ermitteln
-    IFACE=$(ip route | grep default | awk '{print $5}' | head -n 1)
-
-    # Prüfen, ob IFACE leer ist
-    if [ -z "$IFACE" ]; then
-        echo "Kein aktives Netzwerkinterface gefunden."
-        exit 1
-    fi
-
-    # Prüfen, ob Interface-Verzeichnis existiert
-    if [ ! -e "/sys/class/net/$IFACE/address" ]; then
-        echo "Interface $IFACE existiert nicht oder hat keine MAC-Adresse."
-        exit 1
-    fi
-
-    # MAC-Adresse auslesen und ":" entfernen
-    MAC=$(cat /sys/class/net/$IFACE/address | tr -d ':')
-
-    if [ -z "$MAC" ]; then
-        echo "Konnte MAC-Adresse nicht lesen."
-        exit 1
-    fi
-
-    # CLIENT_NAME daraus generieren
-    CLIENT_NAME="picompose-${MAC}"
-
-    # Hostname setzen
-    echo "Setze Hostname auf $CLIENT_NAME"
-    hostnamectl set-hostname "$CLIENT_NAME"
-
-    # Marker-Datei erstellen
-    touch /var/lib/configure_hostname
-
-    # Deaktiviere den Service nach erfolgreicher Konfiguration
-    systemctl disable configure_hostname.service
-}
-
-# Wenn die Konfiguration bereits erfolgreich war, beenden
-if [ -f /var/lib/configure_hostname ]; then
-    exit 0
+# Prüfen, ob IFACE leer ist
+if [ -z "$IFACE" ]; then
+    echo "Kein aktives Netzwerkinterface gefunden."
+    exit 1
 fi
 
-# Versuche die Audio-Einstellungen zu setzen
-configure_hostname
+# Prüfen, ob Interface-Verzeichnis existiert
+if [ ! -e "/sys/class/net/$IFACE/address" ]; then
+    echo "Interface $IFACE existiert nicht oder hat keine MAC-Adresse."
+    exit 1
+fi
+
+# MAC-Adresse auslesen und ":" entfernen
+MAC=$(cat /sys/class/net/$IFACE/address | tr -d ':')
+
+if [ -z "$MAC" ]; then
+    echo "Konnte MAC-Adresse nicht lesen."
+    exit 1
+fi
+
+# CLIENT_NAME daraus generieren
+CLIENT_NAME="picompose-${MAC}"
+
+# Hostname setzen
+echo "Setze Hostname auf $CLIENT_NAME"
+hostnamectl set-hostname "$CLIENT_NAME"
