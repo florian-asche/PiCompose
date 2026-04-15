05c5f74a61d8f1c418e1a46143d26d8a828785d2
Florian Asche <github@florian-asche.de>
2026-04-15 02:31:19 +0200

    add pipewirer runtime directory to systemd

--- 02-stage-audiodriver-respeaker_lite/01-keep-audio-alive/files/keep-audio-alive.sh
+++ 02-stage-audiodriver-respeaker_lite/01-keep-audio-alive/files/keep-audio-alive.sh
@@ -31,8 +31,5 @@ check_pipewire() {
 # Run pipewire check
 check_pipewire
 
-# set custom pipewire path
-export XDG_RUNTIME_DIR=/run/user/1000
-
 # run silent output
 sox -n -r 16000 -c 1 -b 16 -e signed-integer -t alsa pipewire synth 0 sine 0 vol 0.0
