d94a2f2a8a50d7657398ee0ad264d3adda6ff95c
Florian Asche <github@florian-asche.de>
2026-04-19 17:11:54 +0200

    move disable-acp to 2michat only

--- 02-stage-audiodriver-2michat-v1/01-driver/02-run.sh
+++ 02-stage-audiodriver-2michat-v1/01-driver/02-run.sh
@@ -13,3 +13,7 @@ on_chroot << EOF
 systemctl daemon-reload
 systemctl enable seeed-voicecard.service
 EOF
+
+# disable acp
+mkdir -p "${ROOTFS_DIR}/etc/wireplumber/wireplumber.conf.d"
+install -v -m 644 files/51-disable-acp.conf "${ROOTFS_DIR}/etc/wireplumber/wireplumber.conf.d/51-disable-acp.conf"
