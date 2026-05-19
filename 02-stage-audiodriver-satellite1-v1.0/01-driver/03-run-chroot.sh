a80a5a1c1b83246500690b23b2746a378660b059
Florian Asche <github@florian-asche.de>
2026-05-19 12:22:10 +0200

    add final FPH packages path

--- 02-stage-audiodriver-satellite1-v1.0/01-driver/03-run-chroot.sh
+++ 02-stage-audiodriver-satellite1-v1.0/01-driver/03-run-chroot.sh
@@ -8,18 +8,17 @@ echo "Starting satellite1 driver installation..."
 
 # 1. Install the Custom Kernel
 echo "Download and install Custom Kernel"
-# wget https://github.com/florian-asche/Satellite1-RPi/releases/download/develop/linux-headers-6.18.29-fusb302-rpi-v8_2_arm64.deb
-wget https://github.com/florian-asche/Satellite1-RPi/releases/download/develop/linux-image-6.18.29-fusb302-rpi-v8_2_arm64.deb
+wget https://github.com/FutureProofHomes/RPi-Kernel-Fusb302/releases/download/develop/linux-image-6.18.32-fusb302-rpi-v8_2_arm64.deb
 dpkg -i linux-image-*-fusb302-rpi-v8_*_arm64.deb
 
 
 # 2. Install System Configuration
 echo "Download and install System configuration"
-wget https://github.com/florian-asche/Satellite1-RPi/releases/download/develop/satellite1-rpi-setup_1.0-1_arm64.deb
+wget https://github.com/FutureProofHomes/Satellite1-RPi-Setup/releases/download/develop/satellite1-rpi-setup_1.0-1_arm64.deb
 dpkg -i satellite1-rpi-setup_*_arm64.deb
 
 
 # 3. Install the Python SDK
 #echo "Download and install Python SDK"
-wget https://github.com/florian-asche/Satellite1-RPi/releases/download/develop/satellite1-rpi-sdk_0.1.5_arm64.deb
+wget https://github.com/FutureProofHomes/Satellite1-RPi-SDK/releases/download/develop/satellite1-rpi-sdk_0.1.5_arm64.deb
 dpkg -i satellite1-rpi-sdk_*_arm64.deb
